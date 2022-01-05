d7452c67892b398ab762cd0d01c2d5ff427197aa
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-01-05 21:55:45 +0530

    EL Plan genius.sh

---
+++ OffSec/genius.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+### **Make Use Of** **Argument Parser**
+
+# `mkdir` `data_time` & use directory to save the output
+# IF Ports `80, 443, 8000, 8080` THEN  use ZAPROXY & Nikto Scan, Wfuzzer, curl robots.txt
+# ADD to `/etc/hosts`
+# ADD Nmap script engine
+# `nslookup`
+# ADD Hydra Bruteforce `SSH, FTP, Telnet`
+# ADD Auto SSH Login and `GET cat <crucial files>`
+

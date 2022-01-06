07feb0bf408a89c224b8f645da16733bbc721030
aatharvauti <bm.aatharvauti@protonmail.ch>
2022-01-06 09:11:22 +0530

    Completed TODO mkdir data_time & use directory to save the output

--- OffSec/genius.sh
+++ OffSec/genius.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# TODO `mkdir` `data_time` & use directory to save the output
+# // TODO `mkdir` `data_time` & use directory to save the output
+
+# @param FNAME outputs date in mdy_HM format
+FNAME=$(date +%m%d%y_%H%M)
+
+mkdir $HOME/genius 2>/dev/null  # If it already exists, nothing will happen
+mkdir $HOME/genius/$FNAME # Create a folder for storing the output
+# Running this script at 06:09 on 04/20/2022 #* will make a folder named 04202022_0609
+
+echo Output Folder Location: $HOME/genius/$FNAME  # Be nice and tell the user
+cd $HOME/genius/$FNAME
 
 # TODO Make Use Of Argument Parser
 # TODO IF Ports `80, 443, 8000, 8080` THEN  use ZAPROXY & Nikto Scan, Wfuzzer, curl robots.txt
